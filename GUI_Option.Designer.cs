b51efd1853ce495b7e8e0c2bda288e7cd3f9b189
user-ПК\user <griphon56@gmail.com>
2017-12-16 01:10:11 +0300

    Project 1

---
+++ GUI_Option.Designer.cs
@@ -0,0 +1,342 @@
+﻿namespace MailClient
+{
+    partial class GUI_Option
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GUI_Option));
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.labelPassword = new System.Windows.Forms.Label();
+            this.labelUsername = new System.Windows.Forms.Label();
+            this.tbPassword = new System.Windows.Forms.TextBox();
+            this.tbUsername = new System.Windows.Forms.TextBox();
+            this.cbAuth = new System.Windows.Forms.CheckBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbPort = new System.Windows.Forms.TextBox();
+            this.tbHost = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.popUseSSL = new System.Windows.Forms.CheckBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tbPopPassword = new System.Windows.Forms.TextBox();
+            this.tbPopUsername = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.tbPopPort = new System.Windows.Forms.TextBox();
+            this.tbPopHost = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(203, 192);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.labelPassword);
+            this.tabPage1.Controls.Add(this.labelUsername);
+            this.tabPage1.Controls.Add(this.tbPassword);
+            this.tabPage1.Controls.Add(this.tbUsername);
+            this.tabPage1.Controls.Add(this.cbAuth);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.tbPort);
+            this.tabPage1.Controls.Add(this.tbHost);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(195, 166);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "SMTP";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // labelPassword
+            // 
+            this.labelPassword.AutoSize = true;
+            this.labelPassword.Enabled = false;
+            this.labelPassword.Location = new System.Drawing.Point(32, 131);
+            this.labelPassword.Name = "labelPassword";
+            this.labelPassword.Size = new System.Drawing.Size(45, 13);
+            this.labelPassword.TabIndex = 10;
+            this.labelPassword.Text = "Пароль";
+            // 
+            // labelUsername
+            // 
+            this.labelUsername.AutoSize = true;
+            this.labelUsername.Enabled = false;
+            this.labelUsername.Location = new System.Drawing.Point(39, 105);
+            this.labelUsername.Name = "labelUsername";
+            this.labelUsername.Size = new System.Drawing.Size(38, 13);
+            this.labelUsername.TabIndex = 9;
+            this.labelUsername.Text = "Логин";
+            // 
+            // tbPassword
+            // 
+            this.tbPassword.Enabled = false;
+            this.tbPassword.Location = new System.Drawing.Point(83, 128);
+            this.tbPassword.Name = "tbPassword";
+            this.tbPassword.PasswordChar = '*';
+            this.tbPassword.Size = new System.Drawing.Size(100, 20);
+            this.tbPassword.TabIndex = 8;
+            // 
+            // tbUsername
+            // 
+            this.tbUsername.Enabled = false;
+            this.tbUsername.Location = new System.Drawing.Point(83, 102);
+            this.tbUsername.Name = "tbUsername";
+            this.tbUsername.Size = new System.Drawing.Size(100, 20);
+            this.tbUsername.TabIndex = 7;
+            // 
+            // cbAuth
+            // 
+            this.cbAuth.AutoSize = true;
+            this.cbAuth.Location = new System.Drawing.Point(6, 78);
+            this.cbAuth.Name = "cbAuth";
+            this.cbAuth.Size = new System.Drawing.Size(147, 17);
+            this.cbAuth.TabIndex = 6;
+            this.cbAuth.Text = "Требуется авторизация";
+            this.cbAuth.UseVisualStyleBackColor = true;
+            this.cbAuth.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // lbSubject
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 3);
+            this.label3.Name = "lbSubject";
+            this.label3.Size = new System.Drawing.Size(107, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Настройки сервера";
+            // 
+            // tbPort
+            // 
+            this.tbPort.Location = new System.Drawing.Point(83, 52);
+            this.tbPort.Name = "tbPort";
+            this.tbPort.Size = new System.Drawing.Size(100, 20);
+            this.tbPort.TabIndex = 3;
+            // 
+            // tbHost
+            // 
+            this.tbHost.Location = new System.Drawing.Point(83, 26);
+            this.tbHost.Name = "tbHost";
+            this.tbHost.Size = new System.Drawing.Size(100, 20);
+            this.tbHost.TabIndex = 2;
+            // 
+            // lbTo
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(45, 55);
+            this.label2.Name = "lbTo";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Порт";
+            // 
+            // lbFrom
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 29);
+            this.label1.Name = "lbFrom";
+            this.label1.Size = new System.Drawing.Size(76, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "SMTP сервер";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.popUseSSL);
+            this.tabPage2.Controls.Add(this.label4);
+            this.tabPage2.Controls.Add(this.label5);
+            this.tabPage2.Controls.Add(this.tbPopPassword);
+            this.tabPage2.Controls.Add(this.tbPopUsername);
+            this.tabPage2.Controls.Add(this.label6);
+            this.tabPage2.Controls.Add(this.tbPopPort);
+            this.tabPage2.Controls.Add(this.tbPopHost);
+            this.tabPage2.Controls.Add(this.label7);
+            this.tabPage2.Controls.Add(this.label8);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(195, 166);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "POP3";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // popUseSSL
+            // 
+            this.popUseSSL.AutoSize = true;
+            this.popUseSSL.Location = new System.Drawing.Point(6, 131);
+            this.popUseSSL.Name = "popUseSSL";
+            this.popUseSSL.Size = new System.Drawing.Size(122, 17);
+            this.popUseSSL.TabIndex = 21;
+            this.popUseSSL.Text = "Использовать SSL";
+            this.popUseSSL.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(29, 108);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 13);
+            this.label4.TabIndex = 20;
+            this.label4.Text = "Пароль";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(39, 82);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(38, 13);
+            this.label5.TabIndex = 19;
+            this.label5.Text = "Логин";
+            // 
+            // tbPopPassword
+            // 
+            this.tbPopPassword.Location = new System.Drawing.Point(83, 105);
+            this.tbPopPassword.Name = "tbPopPassword";
+            this.tbPopPassword.PasswordChar = '*';
+            this.tbPopPassword.Size = new System.Drawing.Size(100, 20);
+            this.tbPopPassword.TabIndex = 18;
+            // 
+            // tbPopUsername
+            // 
+            this.tbPopUsername.Location = new System.Drawing.Point(83, 79);
+            this.tbPopUsername.Name = "tbPopUsername";
+            this.tbPopUsername.Size = new System.Drawing.Size(100, 20);
+            this.tbPopUsername.TabIndex = 17;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(3, 3);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(107, 13);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Настройки сервера";
+            // 
+            // tbPopPort
+            // 
+            this.tbPopPort.Location = new System.Drawing.Point(83, 52);
+            this.tbPopPort.Name = "tbPopPort";
+            this.tbPopPort.Size = new System.Drawing.Size(100, 20);
+            this.tbPopPort.TabIndex = 14;
+            // 
+            // tbPopHost
+            // 
+            this.tbPopHost.Location = new System.Drawing.Point(83, 26);
+            this.tbPopHost.Name = "tbPopHost";
+            this.tbPopHost.Size = new System.Drawing.Size(100, 20);
+            this.tbPopHost.TabIndex = 13;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(48, 55);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(32, 13);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Порт";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(6, 29);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(74, 13);
+            this.label8.TabIndex = 11;
+            this.label8.Text = "POP3 сервер";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(12, 210);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(199, 23);
+            this.btnSave.TabIndex = 1;
+            this.btnSave.Text = "Сохранить";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // GUI_Option
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(224, 244);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.tabControl1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "GUI_Option";
+            this.Text = "Настройки";
+            this.Load += new System.EventHandler(this.GUI_Option_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Label labelPassword;
+        private System.Windows.Forms.Label labelUsername;
+        private System.Windows.Forms.TextBox tbPassword;
+        private System.Windows.Forms.TextBox tbUsername;
+        private System.Windows.Forms.CheckBox cbAuth;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbPort;
+        private System.Windows.Forms.TextBox tbHost;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tbPopPassword;
+        private System.Windows.Forms.TextBox tbPopUsername;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox tbPopPort;
+        private System.Windows.Forms.TextBox tbPopHost;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.CheckBox popUseSSL;
+    }
+}
